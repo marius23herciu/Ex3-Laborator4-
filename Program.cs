8705509a8334e8f62eb02961cab9401addf956ed
marius.herciu23 <marius.herciu23@gmail.com>
2022-05-30 13:33:07 +0300

    Adaugat functie citire matrice. Calcul corect pentru inmultire matrici.

--- Program.cs
+++ Program.cs
@@ -12,68 +12,72 @@ namespace Ex3_Laborator4_
             //tastaura.Scrieti o functie care va calcula produsul celor doua matrici, apelati-o si afisati-I
             //rezultatul.
 
-            Console.WriteLine("Introduceti numarul de linii pentru fiecare matrice: ");
-            int n = int.Parse(Console.ReadLine());
-            Console.WriteLine("Introduceti numarul de coloane pentru fiecare matrice: ");
-            int m = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Introduceti numarul de linii pentru fiecare matrice: ");
+            //int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Introduceti numarul de coloane pentru fiecare matrice: ");
+            //int m = int.Parse(Console.ReadLine());
 
-            int[,] matrice1 = new int[n, m];
-            Console.WriteLine("Introduceti " + n * m + " numere pentru prima matrice:");
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    matrice1[i, j] = int.Parse(Console.ReadLine());
-                }
-            }
-            int[,] matrice2 = new int[n, m];
-            Console.WriteLine("Introduceti " + n * m + " numere pentru a doua matrice:");
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    matrice2[i, j] = int.Parse(Console.ReadLine());
-                }
-            }
-            int produs = ProdusElemente(matrice1, matrice2);
-            Console.WriteLine("Produsul elementelor celor 2 matrici este: " + produs);
+            //if (n != m)
+            //{
+            //    Console.WriteLine("Numarul de coloane al primei matrice trebuie sa fie egal cu numarul de linii al celei de a doua matrice");
+            //    return;
+            //}
 
-            InmultireElementeCuAceleasiCoordonate(matrice1, matrice2, n, m);
-            Console.WriteLine("Matricea rezultata din inmulitirea elementelor cu aceleasi coordonate este: ");
-            for (int i = 0; i < n; i++)
+            int[,] matrice1 = CitireMatrice();
+            int[,] matrice2 = CitireMatrice();
+
+            if (matrice1.GetLength(1) != matrice2.GetLength(0))
             {
-                for (int j = 0; j < m; j++)
+                Console.WriteLine("Numarul de coloane al primei matrice trebuie sa fie egal cu numarul de linii al celei de a doua matrice");
+                return;
+            }
+
+            int[,] rezultatInmultire = InmultireMatrice(matrice1, matrice2);
+            Console.WriteLine("Matricea rezultata din inmulitirea este: ");
+            for (int i = 0; i < rezultatInmultire.GetLength(0); i++)
+            {
+                for (int j = 0; j < rezultatInmultire.GetLength(1); j++)
                 {
-                    Console.Write(matrice2[i, j] + " ");
+                    Console.Write(rezultatInmultire[i, j] + " ");
                 }
                 Console.WriteLine();
             }
         }
-        static int ProdusElemente(int[,] matrice1, int[,] matrice2)
+        static int[,] CitireMatrice()
         {
-            int produs = 1;
-            foreach (int numar in matrice1)
-            {
-                produs *= numar;
-            }
-            foreach (int numar in matrice2)
-            {
-                produs *= numar;
-            }
+            Console.WriteLine("Introduceti numarul de linii pentru matrice: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti numarul de coloane pentru matrice: ");
+            int m = int.Parse(Console.ReadLine());
 
-            return produs;
-        }
-        static int[,] InmultireElementeCuAceleasiCoordonate(int[,] matrice1, int[,] matrice2, int n, int m)
-        {
+            int[,] matrice = new int[n, m];
+            Console.WriteLine("Introduceti " + n * m + " numere pentru matrice:");
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
                 {
-                    matrice2[i, j] = matrice1[i, j] * matrice2[i, j];
+                    matrice[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            return matrice;
+        }
+        static int[,] InmultireMatrice(int[,] matrice1, int[,] matrice2)
+        {
+            int[,] rezultat = new int[matrice1.GetLength(0), matrice2.GetLength(1)];
+            for (int i = 0; i < matrice1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrice2.GetLength(1); j++)
+                {
+                    int suma = 0;
+                    for (int k = 0; k < matrice2.GetLength(0); k++)
+                    {
+                        suma += matrice1[i, k] * matrice2[k, j];
+                    }
+                    rezultat[i, j] = suma;
                 }
             }
 
-            return matrice2;
+            return rezultat;
         }
     }
 }
